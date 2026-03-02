02a7aca838816fc56747fb78b2e5685a332572a8
Islam Badran <islambadran39@gmail.com>
2026-03-03 01:01:53 +0200

    update apis

--- api/fact_check.js
+++ api/fact_check.js
@@ -4,7 +4,7 @@ export default async function handler(req, res) {
       method: req.method,
       headers: {
         "Content-Type": "application/json",
-        "x-api-key": "fck_nACjLJ0En7jlMRu5S5DH-4P0KW1m5YQodMjsBaJVenfDYJut",
+        "x-api-key": "ata_d96559789f111fffa039143dd65273d1d71a824f998e30b00a26a397",
       },
       body: req.method === "GET" ? undefined : JSON.stringify(req.body),
     });
